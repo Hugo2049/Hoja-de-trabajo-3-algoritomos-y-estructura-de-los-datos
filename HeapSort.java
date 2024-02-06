5410093326cd45b0de9e7395fdb7cc31c402ddd4
Hb_W <hugobarillasvi@hotmail.com>
2024-02-06 16:40:43 -0600

    Adicional al heapsort

--- HeapSort.java
+++ HeapSort.java
@@ -18,4 +18,30 @@ public class HeapSort {
             heapify(arr, i, 0);
         }
     }
+
+    // Para heapify un subárbol enraizado con el nodo i que es
+    // un índice en arr[]. n es el tamaño del heap
+    private static void heapify(int arr[], int n, int i) {
+        int largest = i; // Inicializar largest como raíz
+        int l = 2 * i + 1; // izquierda = 2i + 1
+        int r = 2  i + 2; // derecha = 2*i + 2
+
+        // Si el hijo izquierdo es más grande que la raíz
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        // Si el hijo derecho es más grande que largest hasta ahora
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        // Si largest no es raíz
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Heapify recursivamente el subárbol afectado
+            heapify(arr, n, largest);
+        }
+    }
 }
